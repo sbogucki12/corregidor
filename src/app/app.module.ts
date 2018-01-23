ba597d8ddb9041e15a094ac7cf86ae0e069cd4eb
Steve Bogucki <sbogucki@mail.usf.edu>
2018-01-23 15:16:39 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DiscoverComponent } from './discover/discover.component';
+import { NameComponent } from './name/name.component';
+import { ContactComponent } from './contact/contact.component';
+import { ValuesComponent } from './values/values.component';
+import { BlogComponent } from './blog/blog.component';
+
+const appRoutes: Routes= [
+  {path:"", component: HomeComponent},
+  {path:"discover", component: DiscoverComponent},
+  {path:"name", component: NameComponent},
+  {path:"contact", component: ContactComponent},
+  {path:"values", component: ValuesComponent},
+  {path:"blog", component: BlogComponent}
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DiscoverComponent,
+    NameComponent,
+    ContactComponent,
+    ValuesComponent,
+    BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/name/name.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name',
+  templateUrl: './name.component.html',
+  styleUrls: ['./name.component.css']
+})
+export class NameComponent implements OnInit {
+  crest = '../../assets/images/503rd.jpg';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
